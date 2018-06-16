62a28ded0e16972b31d0f9096fb81a5ae93b91d3
JINSUNG\dnghwls7 <dnghwls7@ajou.ac.kr>
2018-06-16 23:54:15 +0900

    project when 2016 so so easy project

---
+++ Android/AndroidStudioProjects/MyMap/app/build/generated/source/buildConfig/androidTest/debug/com/mymap/littleconan/mymap/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.mymap.littleconan.mymap.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.mymap.littleconan.mymap.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
